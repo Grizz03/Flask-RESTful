183372acc54906edc059c9c8580cb3d51f362d90
Grizz03 <steve.grizz03@gmail.com>
2021-05-23 16:54:39 -0700

    iterate through items

--- code/app.py
+++ code/app.py
@@ -2,17 +2,26 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)  # Easily add Resource's
+api = Api(app)  # To easily add Resource's
+
+# In memory database
+items = []
 
 
-class Student(Resource):
+class Items(Resource):
+    # Iterate through items in the database
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'items': None}, 404  # Not found
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201  # Created successfully
 
 
-api.add_resource(Student, '/student/<string:name>')  # make accessible with API
+api.add_resource(Items, '/item/<string:name>')  # make accessible with API
 
 app.run(port=5000)
-
-
-
