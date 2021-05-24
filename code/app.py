f31e2ad8ce7d34b02a94ab0cb9451ceca8802f55
Grizz03 <steve.grizz03@gmail.com>
2021-05-23 19:09:50 -0700

    error control and improving code

--- code/app.py
+++ code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -8,20 +8,31 @@ api = Api(app)  # To easily add Resource's
 items = []
 
 
-class Items(Resource):
+class Item(Resource):
     # Iterate through items in the database
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'items': None}, 404  # Not found
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {"message': 'An item with name '{}' already exists.".format(name)}, 400
+
+        data = request.get_json(force=True)  # Doesnt need content-type header
+        item = {'name': name, 'price': data['price']}  # Accesses price key of data dictionary
         items.append(item)
         return item, 201  # Created successfully
 
 
-api.add_resource(Items, '/item/<string:name>')  # make accessible with API
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}  # Return dictionary of items
 
-app.run(port=5000)
+
+api.add_resource(Item, '/item/<string:name>')  # make accessible with API
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)  # Debug active
